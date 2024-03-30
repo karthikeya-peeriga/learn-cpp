4cbd23694a6b3fcd437ea5b6b125ae1a59216226
Peeriga Karthikeya <153732471+karthikeya-peeriga@users.noreply.github.com>
2024-03-31 03:11:24 +0530

    added setup and variables

---
+++ setup_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello World\n";
+    cout << "This is CPP\n";
+    cout << "I am going to use it for DSA";
+    return 0;
+}
+
+//This file has introduction, Hello world like code with escape sequencing. 
+
+/*
+This file also contains How to give comments in different ways. 
+*/
---
+++ variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << "Variables" << endl;
+/*
+Vars can be declared by 
+type variable_name = value;
+*/
+    int num = 18;
+    cout << num;
+    return 0;
+}
