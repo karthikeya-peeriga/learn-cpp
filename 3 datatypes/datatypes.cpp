67b01ba36d4a2fd1c01b5132f90a585bf150cdc6
Peeriga Karthikeya <153732471+karthikeya-peeriga@users.noreply.github.com>
2024-04-02 10:48:26 +0530

    added datatypes and use inputs

---
+++ 3 datatypes/datatypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int x = 10;
+    // replacable with the ASCII words
+    char y = 'y';
+
+    string name = "Karthikeya";
+
+    //float allows 6 points, double allows 15
+    double doub = 5.99451561231514;
+    float flt = 0.17361719172;
+
+    cout << flt <<endl<<doub;
+    
+    //1 is true, 0 is false
+    bool phone = "true";
+
+}
